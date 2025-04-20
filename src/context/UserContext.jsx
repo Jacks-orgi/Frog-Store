9c1f6b1dae57effe9589c6070513d7d7dc2ade7a
Dani2051 <dardaniyaal@gmail.com>
2025-04-20 19:59:20 +0100

    New User Context
    User context added to handle logged in and guest users

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const UserContext = createContext();
+
+export const useUser = () => useContext(UserContext);
+
+export const UserProvider = ({ children }) => {
+  const [authToken, setAuthToken] = useState(sessionStorage.getItem('authToken'));
+
+  const login = (token) => {
+    sessionStorage.setItem('authToken', token);
+    setAuthToken(token);
+  };
+
+  const logout = () => {
+    sessionStorage.removeItem('authToken');
+    setAuthToken(null);
+  };
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('authToken');
+    if (token !== authToken) {
+      setAuthToken(token);
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ authToken, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
